254c2854926ccb9e4d0332632b25e96ae3652c23
germmand <german.aguilera17@gmail.com>
2018-04-20 01:21:15 -0400

    Updating GhostsHandler's destructor.

--- PacManAI/GhostsHandler.cpp
+++ PacManAI/GhostsHandler.cpp
@@ -1,11 +1,17 @@
 #include "GhostsHandler.h"
 
 GhostsHandler::GhostsHandler() {
-
+	m_pGhosts = new std::vector<Ghost *>();
 }
 
 GhostsHandler::~GhostsHandler() {
+	for (size_t i = 0; i < m_pGhosts->size(); i++) {
+		delete (*m_pGhosts)[i];
+		(*m_pGhosts)[i] = nullptr;
+	}
 
+	delete m_pGhosts;
+	m_pGhosts = nullptr;
 }
 
 std::vector<Ghost *> *GhostsHandler::getGhosts() {
